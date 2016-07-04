67c2aea1c8546f2a4ee46050320f6be419acdae7
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-07-04 19:30:34 +0000

    Update

--- run.py
+++ run.py
@@ -2,23 +2,37 @@ import oursql
 import datetime
 import ConfigParser
 from mwclient import Site
-import datetime
+
 
 def get_config(fname):
-    config = {}
     parser = ConfigParser.SafeConfigParser()
     parser.read([fname])
     return parser
 
 
-def get_results(config):
+def get_results(config, dabcat):
     rs = []
     db = oursql.connect(**config)
     cursor = db.cursor()
-    cursor.execute("SELECT 'Ancientpages' AS TYPE, page_namespace AS namespace, page_title AS title, UNIX_TIMESTAMP(rev_timestamp) AS VALUE FROM page, revision WHERE page_namespace = 0 AND page_is_redirect = 0 AND page_latest=rev_id ORDER BY VALUE ASC LIMIT 0,2000")
-    # cursor.execute("SELECT 'Ancientpages' AS TYPE, page_namespace AS namespace, page_title AS title, UNIX_TIMESTAMP(rev_timestamp) AS VALUE FROM page, revision LIMIT 0,100")
+    cursor.execute("""SELECT
+      page_title AS title,
+      UNIX_TIMESTAMP(rev_timestamp) AS timestamp,
+      cl_to
+    FROM
+      revision, page
+    LEFT JOIN
+      categorylinks
+      ON cl_from = page_id
+      AND cl_to = ?
+    WHERE
+      page_namespace = 0
+      AND page_is_redirect = 0
+      AND page_latest = rev_id
+    ORDER BY
+      rev_timestamp ASC
+    LIMIT 0,2000""", (dabcat,))
     for row in cursor:
-        rs.append([row[2], row[3]])
+        rs.append([row[0], row[1], row[2] is not None])
     return rs
 
 
@@ -26,12 +40,20 @@ def output_results(results, config):
     txt = u'\n'
     for res in results:
         dt = datetime.datetime.fromtimestamp(int(res[1])).strftime('%F')
-        title = res[0].decode('utf-8')
-        txt += u'# [[%s]] (%s)\n' % (title, dt)
+        title = res[0].decode('utf-8').replace('_', ' ')
+        dab = config['dabdesc'].decode('utf-8')
+        if res[2]:
+            x = u"''[[{}]]'' ({}, {})".format(title, dab, dt)
+        else:
+            x = u"[[{}]] ({})".format(title, dt)
+        txt += u'# {}\n'.format(x)
 
     site = Site(('https', config['host']), clients_useragent=config['clients_useragent'])
     site.login(config['user'], config['passwd'])
-    page = site.pages[config['page']]
+    pagename = config['page'].decode('utf-8')
+    page = site.pages[pagename]
+    if not page.exists:
+        raise StandardError(u"Page does not exist")
 
     oldtxt = page.text()
     tagstart = u'<!--Bot:StartListe-->'
@@ -39,20 +61,20 @@ def output_results(results, config):
     if posstart == -1:
         raise StandardError("Fant ikke tagstart")
     posstart += len(tagstart)
-    
+
     txt = oldtxt[:posstart] + txt
-    page.save(txt, config['summary'])
+    page.save(txt, config['summary'].decode('utf-8'))
 
 
 a = datetime.datetime.now()
 
 for cnf in ['config.no.cnf', 'config.nn.cnf']:
+    print 'Using: {}'.format(cnf)
     config = get_config(cnf)
     dbconf = dict(config.items('db'))
     mwconf = dict(config.items('mw'))
-    output_results(get_results(dbconf), mwconf)
+    output_results(get_results(dbconf, mwconf['dabcat'].decode('utf-8')), mwconf)
 
 b = datetime.datetime.now()
 c = b - a
 print "Completed in %d seconds" % (c.total_seconds())
-
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "=== This is DanmicholoBot running on $(hostname) ==="
-
+date
 . ENV/bin/activate
 python run.py
 
